f82dc15ffcd397e6fe49de8f836d1dfce867b5d3
Allison Homem <mrs.allisonhomem@outlook.com>
2021-10-12 21:28:08 -0500

    finished users model function find

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,6 +1,8 @@
 const db = require('../../data/db-config.js');
 
 function find() {
+  return db('users').join('roles', 'users.role_id', 'roles.role_id')
+                    .select('user_id', 'username', 'role_name')
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users.
